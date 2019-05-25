f50b8e71a0d25b579c8c74d707f3fb9a0deeec95
Kat B <katreesebarnes@gmail.com>
2019-05-25 23:46:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,7 +100,7 @@ class Student
        FROM students 
        WHERE grade = 10
        ORDER BY students.id
-       LIMIT 1 
+       LIMIT 1; 
       SQL
       
        DB[:conn].execute(sql).map do |row|
