3cc29a6e9969862769d064686ea453ff55f7fa9f
Kat B <katreesebarnes@gmail.com>
2019-05-25 23:39:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,14 +86,12 @@ class Student
        SELECT *
        FROM students 
        WHERE grade = 10
-       ORDER BY students.id; 
-       SQL 
+       ORDER BY students.id
+       LIMIT ? 
+      SQL
        
         DB[:conn].execute(sql).map do |row|
     self.new_from_db(row)
     end
-       
-       
-    
   end
 end
